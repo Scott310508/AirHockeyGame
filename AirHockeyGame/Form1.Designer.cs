c84dc7e1db5122fa81584931f89250eb480a7eb8
Scott Ngo <anhtngo124@ed.amdsb.ca>
2025-05-29 14:07:12 -0400

    Add project files.

---
+++ AirHockeyGame/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace AirHockeyGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTitlePicture = new System.Windows.Forms.PictureBox();
+            this.textBox = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player2Label = new System.Windows.Forms.Label();
+            this.player1Label = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.quitButton = new AirHockeyGame.RJControls.RJButton();
+            this.startButton = new AirHockeyGame.RJControls.RJButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gameTitlePicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTitlePicture
+            // 
+            this.gameTitlePicture.BackColor = System.Drawing.Color.Transparent;
+            this.gameTitlePicture.Image = global::AirHockeyGame.Properties.Resources.air_hockey_logo;
+            this.gameTitlePicture.Location = new System.Drawing.Point(12, 12);
+            this.gameTitlePicture.Name = "gameTitlePicture";
+            this.gameTitlePicture.Size = new System.Drawing.Size(562, 289);
+            this.gameTitlePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.gameTitlePicture.TabIndex = 1;
+            this.gameTitlePicture.TabStop = false;
+            // 
+            // textBox
+            // 
+            this.textBox.AutoSize = true;
+            this.textBox.BackColor = System.Drawing.Color.Transparent;
+            this.textBox.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.ForeColor = System.Drawing.Color.White;
+            this.textBox.Location = new System.Drawing.Point(130, 623);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(308, 20);
+            this.textBox.TabIndex = 5;
+            this.textBox.Text = "Scott\'s Airhockey 2025. Style unmatched. ";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 30;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // player2Label
+            // 
+            this.player2Label.AutoSize = true;
+            this.player2Label.BackColor = System.Drawing.Color.Transparent;
+            this.player2Label.Font = new System.Drawing.Font("Bahnschrift", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2Label.ForeColor = System.Drawing.Color.White;
+            this.player2Label.Location = new System.Drawing.Point(24, 260);
+            this.player2Label.Name = "player2Label";
+            this.player2Label.Size = new System.Drawing.Size(48, 54);
+            this.player2Label.TabIndex = 10;
+            this.player2Label.Text = "0";
+            this.player2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.player2Label.Visible = false;
+            // 
+            // player1Label
+            // 
+            this.player1Label.AutoSize = true;
+            this.player1Label.BackColor = System.Drawing.Color.Transparent;
+            this.player1Label.Font = new System.Drawing.Font("Bahnschrift", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1Label.ForeColor = System.Drawing.Color.White;
+            this.player1Label.Location = new System.Drawing.Point(24, 376);
+            this.player1Label.Name = "player1Label";
+            this.player1Label.Size = new System.Drawing.Size(48, 54);
+            this.player1Label.TabIndex = 11;
+            this.player1Label.Text = "0";
+            this.player1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.player1Label.Visible = false;
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Bahnschrift", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(125, 304);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(48, 54);
+            this.winLabel.TabIndex = 12;
+            this.winLabel.Text = "0";
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winLabel.Visible = false;
+            // 
+            // quitButton
+            // 
+            this.quitButton.BackColor = System.Drawing.Color.Black;
+            this.quitButton.BackgroundColor = System.Drawing.Color.Black;
+            this.quitButton.BorderColor = System.Drawing.Color.Black;
+            this.quitButton.BorderRadius = 35;
+            this.quitButton.BorderSize = 0;
+            this.quitButton.FlatAppearance.BorderSize = 0;
+            this.quitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.quitButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitButton.ForeColor = System.Drawing.Color.Orange;
+            this.quitButton.Location = new System.Drawing.Point(199, 440);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(176, 60);
+            this.quitButton.TabIndex = 9;
+            this.quitButton.Text = "QUIT";
+            this.quitButton.TextColor = System.Drawing.Color.Orange;
+            this.quitButton.UseVisualStyleBackColor = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.BackgroundColor = System.Drawing.Color.Black;
+            this.startButton.BorderColor = System.Drawing.Color.Black;
+            this.startButton.BorderRadius = 35;
+            this.startButton.BorderSize = 0;
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Orange;
+            this.startButton.Location = new System.Drawing.Point(199, 331);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(176, 60);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "START";
+            this.startButton.TextColor = System.Drawing.Color.Orange;
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::AirHockeyGame.Properties.Resources.pattern_seamless_memphis_geometric_graphic_pattern_136321_786;
+            this.ClientSize = new System.Drawing.Size(592, 652);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.player1Label);
+            this.Controls.Add(this.player2Label);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.gameTitlePicture);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Air Hockey Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.gameTitlePicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox gameTitlePicture;
+        private System.Windows.Forms.Label textBox;
+        private System.Windows.Forms.Timer gameTimer;
+        private RJControls.RJButton startButton;
+        private RJControls.RJButton quitButton;
+        private System.Windows.Forms.Label player2Label;
+        private System.Windows.Forms.Label player1Label;
+        private System.Windows.Forms.Label winLabel;
+    }
+}
+
---
+++ AirHockeyGame/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AirHockeyGame
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(185, 385, 65, 65);
+        Rectangle player2 = new Rectangle(185, 105, 65, 65);
+        Rectangle puck = new Rectangle(200, 262, 35, 35);
+        Rectangle invline1 = new Rectangle(150, 550, 130, 550);
+        Rectangle invline2 = new Rectangle(150, 6, 130, 6);
+
+        int playerSpeed = 6;
+        int puckXSpeed = 12;
+        int puckYSpeed = 12;
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        Random randGen = new Random();
+        SoundPlayer player = new SoundPlayer();
+        Pen bluePen = new Pen(Color.Blue, 18);
+        Pen redPen = new Pen(Color.Red, 18);
+        Pen greenPen = new Pen(Color.Green, 18);
+        Pen orangePen = new Pen(Color.Orange, 18);
+        Pen blackPen = new Pen(Color.Black, 18);
+        Pen whitePen = new Pen(Color.White, 2);
+        SolidBrush lightblueBrush = new SolidBrush(Color.LightSkyBlue);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush lightgreenBrush = new SolidBrush(Color.LightGreen);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        bool wDown = false;
+        bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool rightArrowDown = false;
+        bool leftArrowDown = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(450, 600);
+            player = new SoundPlayer(Properties.Resources.theme_song);
+            player.Play();
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            gameTitlePicture.Visible = false;
+            startButton.Visible = false;
+            quitButton.Visible = false;
+            textBox.Visible = false;
+            this.BackgroundImage = null;
+            player2Label.Visible = true;
+            player1Label.Visible = true;
+            gameTimer.Enabled = true;
+            //this.Paint += new PaintEventHandler(Form1_Paint);
+            //this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            //this.StartPosition = FormStartPosition.CenterScreen;
+            //Timer timer = new Timer();
+            //timer.Interval = 16;
+            //timer.Tick += new EventHandler(OnTimerClick);
+            //timer.Start();
+            this.Focus();   
+
+        }
+
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            if (gameTimer.Enabled)
+            {
+                e.Graphics.DrawRectangle(blackPen, 0, 0, 450, 600);
+                e.Graphics.FillRectangle(blackBrush,0, 0,450,600); 
+                e.Graphics.DrawArc(whitePen, 143, -73, 148, 148, 0, 180);
+                e.Graphics.DrawArc(whitePen, 143, 485, 148, 148, 0, -180);
+                e.Graphics.DrawArc(whitePen, 143, 206, 148, 148, 0, 360);
+                e.Graphics.DrawLine(whitePen, 0, 280, 450, 280);
+
+                //player 1
+                e.Graphics.FillEllipse(lightblueBrush, player1);
+                //player2
+                e.Graphics.FillEllipse(redBrush, player2);
+                //puck
+                e.Graphics.FillEllipse(lightgreenBrush, puck);
+
+                //borderlines
+                e.Graphics.DrawLine(greenPen, 0, 0, (this.Width /2) - 80, 0);
+                e.Graphics.DrawLine(greenPen, 0, 0, 0, this.Height / 2);
+                e.Graphics.DrawLine(orangePen, 0, 560, (this.Width / 2) - 80, 560);
+                e.Graphics.DrawLine(orangePen, 0, 600, 0, (this.Height / 2) - 20);
+                e.Graphics.DrawLine(redPen, this.Width, 0, (this.Width / 2) + 65, 0);
+                e.Graphics.DrawLine(redPen, 432, 0, 432, this.Height / 2);
+                e.Graphics.DrawLine(bluePen, 450, 560, (this.Width / 2) + 65, 560);
+                e.Graphics.DrawLine(bluePen, 432, 600, 432, (this.Height / 2) - 20);
+            }
+        }
+
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            
+            //move player1
+            if (wDown == true && player1.Y >= 285)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sDown == true && player1.Y < 480)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            if (aDown == true && player1.X >= 15)
+            {
+                player1.X -= playerSpeed;
+            }
+
+            if (dDown == true && player1.X <= 353)
+            {
+                player1.X += playerSpeed;
+            }
+
+            //move  player 2 
+            if (upArrowDown == true && player2.Y > 10)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if (downArrowDown == true && player2.Y <= 205)
+            {
+                player2.Y += playerSpeed;
+            }
+
+            if (rightArrowDown == true && player2.X <= 353)
+            {
+                player2.X += playerSpeed;
+            }
+
+            if (leftArrowDown == true && player2.X >= 15)
+            {
+                player2.X -= playerSpeed;
+            }
+
+            //move puck
+            puck.X += puckXSpeed;
+            puck.Y += puckYSpeed;
+
+            //check if ball hit the side walls and change direction if it does
+            if (puck.X <= 5 || puck.X > this.Width - puck.Width - 20)
+            {
+                player = new SoundPlayer(Properties.Resources.puck_hit);
+                player.Play();
+                puckXSpeed *= -1; 
+            }
+
+            //check if ball hit the top and bottom walls and change direction if it does
+            if (puck.Y <= 10 || puck.Y > this.Height - puck.Height - 50)
+            {
+                player = new SoundPlayer(Properties.Resources.puck_hit);
+                player.Play();
+                puckYSpeed *= -1;
+            }
+
+
+            //check if ball hits either player. If it does change the direction 
+            //and place the ball in front of the player hit 
+            if (player1.IntersectsWith(puck))
+            {
+                puckXSpeed *= -1;
+                player = new SoundPlayer(Properties.Resources.puck_hit);
+                player.Play();
+            }
+            else if (player2.IntersectsWith(puck))
+            {
+                puckXSpeed *= -1;
+                player = new SoundPlayer(Properties.Resources.puck_hit);
+                player.Play();
+            }
+
+
+
+
+            //check if a player missed the ball and if true add 1 to score of other player  
+            if (puck.IntersectsWith(invline2))
+            {
+            player1Score++;
+            player1Label.Text = $"{player1Score}";
+
+            }
+            else if (puck.Y > 600)
+            {
+                player2Score++;
+                player2Label.Text = $"{player2Score}";
+
+            }
+
+            if (puck.IntersectsWith(invline1))
+            {
+                player2Score++;
+                player2Label.Text = $"{player2Score}";
+
+            }
+            // check score and stop game if either player is at 3 
+            if (player2Score == 5)
+            {
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                player1Label.Visible = false;
+                player2Label.Visible = false;
+                winLabel.Text = "Player  2 Wins!!";
+            }
+            else if (player1Score == 5)
+            {
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                player1Label.Visible = false;
+                player2Label.Visible = false;
+                winLabel.Text = "Player  1 Wins!!";
+            }
+
+            Refresh();
+
+        }
+
+
+        private void Form1_KeyDown(Object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+            }
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+            }
+        }
+    }
+}
